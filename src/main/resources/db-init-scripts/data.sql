83913ce08aa42cd0e29eea4c8311c3c40f9b7ae5
Yu Jin <qldls0307@naver.com>
2024-05-20 16:57:13 +0900

    add: sql setting

---
+++ src/main/resources/db-init-scripts/data.sql
@@ -0,0 +1,6 @@
+insert into shortened_url (short_url, origin_url, created_at) values
+('abc', 'http://example.com/page1', '2024-04-01 10:00:00'),
+('def', 'http://example.com/page2', '2024-04-02 12:00:00'),
+('ghi', 'http://example.com/page3', '2024-04-03 14:00:00'),
+('jkl', 'http://example.com/page4', '2024-04-04 16:00:00'),
+('mno', 'http://example.com/page5', '2024-04-05 18:00:00');
