2c57b32a592d54f11e283239787cdaeb1bfef5e1
Yu Jin <qldls0307@naver.com>
2024-05-20 16:59:20 +0900

    feat: controller, service 구현

--- src/main/java/com/example/urlshortener/domain/url/UrlService.java
+++ src/main/java/com/example/urlshortener/domain/url/UrlService.java
@@ -4,21 +4,25 @@ import com.example.urlshortener.common.utils.RandomStringUtil;
 import com.example.urlshortener.domain.url.dto.ShortenedUrlDto;
 import com.example.urlshortener.domain.url.entity.ShortenedUrl;
 import com.example.urlshortener.domain.url.exception.UrlNotFoundException;
+import com.example.urlshortener.domain.url.repository.ShortenedUrlQueryRepository;
 import com.example.urlshortener.domain.url.repository.ShortenedUrlRepository;
 import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class UrlService {
+
     private final ShortenedUrlRepository shortenedUrlRepository;
+    private final ShortenedUrlQueryRepository shortenedUrlQueryRepository;
 
     @Transactional
     public ShortenedUrlDto createShortUrl(String url) {
-        ShortenedUrl existingShortenedUrl = shortenedUrlRepository.findByOriginUrl(url).orElse(null);
+        ShortenedUrl existingShortenedUrl = shortenedUrlRepository.findByOriginUrl(url)
+            .orElse(null);
 
         if (existingShortenedUrl != null) {
             return ShortenedUrlDto.from(existingShortenedUrl);
@@ -36,7 +40,8 @@ public class UrlService {
         ShortenedUrl shortenedUrl = shortenedUrlRepository.findByShortUrl(shortId)
             .orElseThrow(UrlNotFoundException::new);
 
-        return new ShortenedUrlDto(shortenedUrl.getId(), shortenedUrl.getShortUrl(), shortenedUrl.getOriginUrl(), shortenedUrl.getCreatedAt());
+        return new ShortenedUrlDto(shortenedUrl.getId(), shortenedUrl.getShortUrl(),
+            shortenedUrl.getOriginUrl(), shortenedUrl.getCreatedAt());
     }
 
     public String getOriginUrl(String shortId) {
@@ -44,5 +49,17 @@ public class UrlService {
             .orElseThrow(UrlNotFoundException::new);
 
         return shortenedUrl.getOriginUrl();
+    }
+
+
+    public List<ShortenedUrlDto> getShortUrlsWithJpa(String inquiry) {
+        List<ShortenedUrl> shortenedUrls = shortenedUrlRepository.findAllByOriginUrlContains(
+            inquiry);
+
+        return ShortenedUrlDto.from(shortenedUrls);
+    }
+
+    public List<ShortenedUrlDto> getShortUrlsWithQueryDsl(String inquiry) {
+        return shortenedUrlQueryRepository.getShortUrlsWithQueryDsl(inquiry);
     }
 }
--- src/main/java/com/example/urlshortener/domain/url/controller/UrlController.java
+++ src/main/java/com/example/urlshortener/domain/url/controller/UrlController.java
@@ -8,8 +8,16 @@ import com.example.urlshortener.domain.url.dto.ShortenedUrlDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotBlank;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RequiredArgsConstructor
@@ -33,19 +41,19 @@ public class UrlController {
         return Response.data(ShortUrlResponse.from(shortenedUrl));
     }
 
-    @Operation(
-        summary = "단축 URL 조회하기",
-        responses = {
-            @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "404", description = "URL_NOT_FOUND"),
-            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
-        }
-    )
-    @GetMapping("/{short_id}")
-    public Response<ShortUrlResponse> getShortUrl(@PathVariable("short_id") String shortId) {
-        ShortenedUrlDto shortenedUrl = urlService.getShortUrl(shortId);
-        return Response.data(ShortUrlResponse.from(shortenedUrl));
-    }
+//    @Operation(
+//        summary = "단축 URL 조회하기",
+//        responses = {
+//            @ApiResponse(responseCode = "200", description = "OK"),
+//            @ApiResponse(responseCode = "404", description = "URL_NOT_FOUND"),
+//            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+//        }
+//    )
+//    @GetMapping("/{short_id}")
+//    public Response<ShortUrlResponse> getShortUrl(@PathVariable("short_id") String shortId) {
+//        ShortenedUrlDto shortenedUrl = urlService.getShortUrl(shortId);
+//        return Response.data(ShortUrlResponse.from(shortenedUrl));
+//    }
 
     @Operation(
         summary = "Short URL 리디렉션",
@@ -60,5 +68,38 @@ public class UrlController {
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl(originUrl);
         return redirectView;
+    }
+
+    /**
+     * 4주차 과제입니다.
+     */
+
+    @Operation(
+        summary = "JPA로 URL 조회하기",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+        }
+    )
+    @GetMapping("/jpa")
+    public Response<List<ShortenedUrlDto>> getShortUrlsWithJpa(
+        @NotBlank @RequestParam("inquiry") String inquiry) {
+        List<ShortenedUrlDto> shortenedUrls = urlService.getShortUrlsWithJpa(inquiry);
+        return Response.data(shortenedUrls);
+    }
+
+    @Operation(
+        summary = "QueryDsl로 URL 조회하기",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+        }
+    )
+
+    @GetMapping("/query-dsl")
+    public Response<List<ShortenedUrlDto>> getShortUrl(
+        @NotBlank @RequestParam("inquiry") String inquiry) {
+        List<ShortenedUrlDto> shortenedUrls = urlService.getShortUrlsWithQueryDsl(inquiry);
+        return Response.data(shortenedUrls);
     }
 }
